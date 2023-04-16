ed76b06d7fc595071fcf41732d9b4c575af0a070
zieDoo <samuel.bednarikall@gmail.com>
2023-04-16 20:18:22 +0200

    pridavam welcome.php screen do zalohvacieho systemu

---
+++ welcome.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <link rel="stylesheet" type="text/css" href="styles.css" />
+    <script src="functions.js"></script>
+</head>
+
+<body>
+
+<div id="inline">
+    <div class="one">
+        <h1>Game - Auto_Bot</h1>
+    </div>
+    <div class="two">
+        <button id="login" onclick="login_function()">Login</button>
+    </div>
+</div>
+
+<hr id="divider">
+
+
+</body>
+
